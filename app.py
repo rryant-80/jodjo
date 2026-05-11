5fd76002a2409e4a9a54f522e04fc609393c8254
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-05-11 11:42:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-url = "https://docs.google.com/spreadsheets/d/15ud5cMl-Pk_uh2tSG_MfwIyAlPMvZNxg1UehegsbHA4/edit"
-df_order = conn.read(spreadsheet=url, worksheet="order")
+df_order = conn.read(
+    spreadsheet="https://docs.google.com/spreadsheets/d/15ud5cMl-Pk_uh2tSG_MfwIyAlPMvZNxg1UehegsbHA4/edit",
+    worksheet="order"
