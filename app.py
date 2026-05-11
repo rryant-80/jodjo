c25fd4a4c3ad630cb56e03a63674e292c5470d5c
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-05-11 14:31:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
-from st_gsheets_connection import GSheetsConnection
+# Coba gunakan cara import ini jika cara pertama gagal terus
+from streamlit_gsheets import GSheetsConnection 
+
 conn = st.connection("gsheets", type=GSheetsConnection)
+
 df_order = conn.read(
     spreadsheet="https://docs.google.com/spreadsheets/d/15ud5cMl-Pk_uh2tSG_MfwIyAlPMvZNxg1UehegsbHA4/edit",
     worksheet="order"
