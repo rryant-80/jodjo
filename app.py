f40d460bc2caf339e97c0eb138a297758e708b44
rryant-80 <67359656+rryant-80@users.noreply.github.com>
2026-05-11 14:25:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 import streamlit as st
 from st_gsheets_connection import GSheetsConnection
-
-# --- BARIS YANG HARUS DITAMBAHKAN ---
-# Ini memberikan instruksi kepada Streamlit untuk membuat objek koneksi bernama 'conn'
 conn = st.connection("gsheets", type=GSheetsConnection)
-# ------------------------------------
-
-# Sekarang 'conn' sudah ada, baris di bawah ini tidak akan error lagi
 df_order = conn.read(
     spreadsheet="https://docs.google.com/spreadsheets/d/15ud5cMl-Pk_uh2tSG_MfwIyAlPMvZNxg1UehegsbHA4/edit",
     worksheet="order"
